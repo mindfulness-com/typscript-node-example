52e9b2d9d2ba66546eb6937d6559982ec1fb9f6b
Sebastian Kade <sebastian@bitbound.com.au>
2019-02-07 13:49:30 +0900

    Typescript graphql endpoint deployed to now

---
+++ index.ts
@@ -0,0 +1,42 @@
+import { GraphQLServer } from "graphql-yoga";
+
+const PORT = process.env.port || 3000;
+
+const typeDefs = `
+  type Query {
+    company(id: Int): Company
+    companies: [Company]
+  }
+
+  type Company {
+    id: Int
+    name: String
+  }
+`;
+
+const companies = {
+  1: {
+    id: 1,
+    name: "Super BV"
+  },
+  2: {
+    id: 2,
+    name: "Uber BV"
+  },
+  3: {
+    id: 3,
+    name: "Terra BV"
+  }
+};
+
+const resolvers = {
+  Query: {
+    company: (_, { id }) => companies[id],
+    companies: () => Object.keys(companies).map(key => companies[key])
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start({ port: PORT }, () =>
+  console.log(`Server is running on localhost:${PORT}`)
+);
